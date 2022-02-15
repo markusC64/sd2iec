6f0a0aaa6ac0a6718cf64156476dc7862af63d34
Martin Thierer <mthierer@gmail.com>
2022-02-15 22:19:25 +0100

    Enable hidden files in D64 images
    Treats directory entries with a zero file type but track != 0 as
    "hidden". These entries are then included in the enumeration by
    next_match() if called with FLAG_HIDDEN set for the filetype.
    
    Has to be enabled by setting the d64dh.hidden flag *after* calling
    opendir().
    
    Warning: As d64dh is the member of a *union* in dh_t, this is only
    valid after confirming that a D64 image is actually mounted.
    Otherwise this will garble the active directory handle!

--- src/d64ops.c
+++ src/d64ops.c
@@ -1293,6 +1293,7 @@ static uint8_t d64_opendir(dh_t *dh, path_t *path) {
   dh->dir.d64.track  = path->dir.dxx.track;
   dh->dir.d64.sector = path->dir.dxx.sector;
   dh->dir.d64.entry  = 0;
+  dh->dir.d64.hidden = 0;
 
   if (partition[path->part].imagetype == D64_TYPE_DNP) {
     /* Read the real first directory sector from the header sector */
@@ -1318,6 +1319,10 @@ static int8_t d64_readdir(dh_t *dh, cbmdirent_t *dent) {
 
     if (ops_scratch[DIR_OFS_FILE_TYPE] != 0)
       break;
+
+    /* Treat file type 0 as hidden file if requested by flag and track != 0 */
+    if (ops_scratch[DIR_OFS_TRACK] != 0 && dh->dir.d64.hidden)
+      break;
   } while (1);
 
   memset(dent, 0, sizeof(cbmdirent_t));
@@ -1325,6 +1330,9 @@ static int8_t d64_readdir(dh_t *dh, cbmdirent_t *dent) {
   dent->opstype = OPSTYPE_DXX;
   dent->typeflags = (ops_scratch[DIR_OFS_FILE_TYPE] ^ FLAG_SPLAT)
     & ~FLAG_HIDDEN;
+  /* Treat entries with type == 0 as hidden */
+  if (dent->typeflags == 0)
+    dent->typeflags = FLAG_HIDDEN;
 
   if ((dent->typeflags & TYPE_MASK) > TYPE_DIR)
     /* Change invalid types to DEL */
